83429937c813e31812fedfc9d7c38a4bf3e75e9f
Patrick Boe <patrick@hametz>
2015-07-11 05:26:55 -0400

    added naming into rewrite pipeline

--- transform/src/tool.clj
+++ transform/src/tool.clj
@@ -6,17 +6,37 @@
     [clojure.java.io :as io]
     [net.cgrand.enlive-html :as en]))
 
+;;rendering
+
 (defn render [n] (apply str (en/emit* n)))
 
-(def rewrite-mobile-chapter (rewrite-chapter "m.joyceproject.com" link-codes))
+(defn rerender [f]
+  (comp render f en/html-resource))
 
-(defn chapters [root]
-  (filter
-    #(.endsWith (.getName %) ".php")
-    (file-seq (io/file (str root "/chap/")))))
+;;routing
+
+(defn route-chapter [x] "something")
+
+(defn source-chapters [s] (str s "/chap/"))
+
+;;io
+
+(defstruct finfo :name :content)
+
+(defn get-info [f] (struct finfo (.getName f) f))
+
+(defn read-contents [dir]
+  (map get-info (rest (file-seq (io/file dir)))))
+
+;;composition
 
 (def source "/home/patrick/dev/proj/joyceproject_archive")
 
-(def rerender (comp render rewrite-mobile-chapter en/html-resource))
+(def rerender-chapter
+  (rerender (rewrite-chapter "m.joyceproject.com" link-codes)))
 
-(def rewritten (map rerender (chapters source)))
+(defn divert [{n :name c :content}]
+  (struct finfo (route-chapter n) (rerender-chapter c)))
+
+(def rewritten
+  (map divert (read-contents (source-chapters source))))
