bea7c0223b4c08ea2ed5a2dc36653bee03eee367
Patrick Boe <patrick@hametz>
2015-11-14 23:39:43 -0500

    added a monitor for scrolling away from the menu so that it hides faster

--- src/jpmobile/script/ui.js
+++ src/jpmobile/script/ui.js
@@ -46,72 +46,101 @@ module.exports =
               return pixelsMoved > 30
             },
 
-            scrollUpMonitor = function(onScroll){
-              var scrollY = window.scrollY,
+            up = true,
 
-                scrollMonitor = function(){
-                  var prevY = scrollY;
-                  scrollY = window.scrollY;
-                  if(scrollY < prevY) { onScroll(); }
-                },
+            down = !up,
 
-                dampenedScrollMonitor = dampen(scrollMonitor,30),
+            scrollMonitor = function(direction) {
+              var progressing =
+                direction === up ?
+                function(y0,y1) { return y0 > y1; } :
+                function(y0,y1) { return y0 < y1; };
 
-                self = {
-                  dispose : function(){
-                    window.removeEventListener("scroll",dampenedScrollMonitor.execute);
-                    dampenedScrollMonitor.dispose();
-                  }
-                }
-              window.addEventListener('scroll', dampenedScrollMonitor.execute);
+              return function(onScroll){
+                var prevY = window.scrollY,
 
-              return self;
-            },
+                  mon = function(){
+                    if(progressing(prevY, window.scrollY)) { onScroll(); }
+                    prevY = window.scrollY;
+                  },
 
-            swipeDownMonitor = function(onSwipe){
-              var highestTouch = NaN,
-                  moveMon = toSingleTouchHandler(function(e){
-                    var y = e.changedTouches[0].clientY;
-                    if(isNaN(highestTouch) || y < highestTouch) {
-                      highestTouch=y;
-                    } else if(constitutesASwipe(y - highestTouch)) {
-                      onSwipe();
-                    }
-                  }),
-                  startMon = toSingleTouchHandler(function(e){
-                    highestTouch=e.changedTouches[0].clientY;
-                  }),
-                  endMon = toSingleTouchHandler(function(e) {
-                    highestTouch=NaN;
-                  }),
+                  dampenedScrollMonitor = dampen(mon,30),
+
                   self = {
-                    dispose: function(){
-                      main.removeEventListener('touchmove', moveMon);
-                      main.removeEventListener('touchstart', startMon);
-                      main.removeEventListener('touchend', endMon);
-                      main.removeEventListener('touchcancel', endMon);
+                    dispose : function(){
+                      window.removeEventListener("scroll",dampenedScrollMonitor.execute);
+                      dampenedScrollMonitor.dispose();
                     }
-                  };
-              main.addEventListener('touchmove', moveMon);
-              main.addEventListener('touchstart', startMon);
-              main.addEventListener('touchend', endMon);
-              main.addEventListener('touchcancel', endMon);
-              return self;
-            },
-
-            menuSeekingMonitor = function(onSeek){
-                var swipeMon = swipeDownMonitor(onSeek),
-                    scrollMon = scrollUpMonitor(onSeek),
-
-                self = {
-                  dispose : function(){
-                    swipeMon.dispose();
-                    scrollMon.dispose();
                   }
-                };
+                window.addEventListener('scroll', dampenedScrollMonitor.execute);
 
                 return self;
+              };
             },
+
+            swipeMonitor = function(direction) {
+              var metrics =
+                direction === up ?
+
+                { isNewExtreme : function(extreme, cur) { return cur > extreme; },
+                  magnitude : function(extreme, cur) { return extreme - cur; } } :
+
+                { isNewExtreme : function(extreme, cur) { return cur < extreme; },
+                  magnitude : function(extreme, cur) { return cur - extreme; } };
+
+              return function(onSwipe){
+                var extremeTouch = NaN,
+                    moveMon = toSingleTouchHandler(function(e){
+                      var y = e.changedTouches[0].clientY;
+                      if(isNaN(extremeTouch) || metrics.isNewExtreme(extremeTouch, y)) {
+                        extremeTouch=y;
+                      } else if(constitutesASwipe(metrics.magnitude(extremeTouch,y))) {
+                        onSwipe();
+                      }
+                    }),
+                    startMon = toSingleTouchHandler(function(e){
+                      extremeTouch=e.changedTouches[0].clientY;
+                    }),
+                    endMon = toSingleTouchHandler(function(e) {
+                      extremeTouch=NaN;
+                    }),
+                    self = {
+                      dispose: function(){
+                        main.removeEventListener('touchmove', moveMon);
+                        main.removeEventListener('touchstart', startMon);
+                        main.removeEventListener('touchend', endMon);
+                        main.removeEventListener('touchcancel', endMon);
+                      }
+                    };
+                main.addEventListener('touchmove', moveMon);
+                main.addEventListener('touchstart', startMon);
+                main.addEventListener('touchend', endMon);
+                main.addEventListener('touchcancel', endMon);
+                return self;
+              };
+            },
+
+            lookingMonitor = function(direction) {
+              var makeSwipeMonitor = swipeMonitor(!direction),
+                  makeScrollMonitor = scrollMonitor(direction);
+              return function(onSeek){
+                  var swipeMon = makeSwipeMonitor(onSeek),
+                      scrollMon = makeScrollMonitor(onSeek),
+
+                  self = {
+                    dispose : function(){
+                      swipeMon.dispose();
+                      scrollMon.dispose();
+                    }
+                  };
+
+                  return self;
+              }
+            },
+
+            lookingDownMonitor = lookingMonitor(down),
+
+            lookingUpMonitor = lookingMonitor(up),
 
             navigating = function(){
               var self = {
@@ -130,15 +159,25 @@ module.exports =
             },
 
             browsing = function(){
-              var setIdle = function(){ processUIEvent("idle") }
+              var setIdle = function(){ processUIEvent("idle") },
+                seekMon = lookingDownMonitor(function(){
+                  processUIEvent("seeking text")
+                }),
                 idleTimeout = window.setTimeout(setIdle, 2500),
                 self = {
                   transition : function(event){
                     switch(event) {
                       case "user toggle":
+                        seekMon.dispose();
                         window.clearTimeout(idleTimeout);
                         return navigating();
-                      case "idle" : return reading();
+                      case "idle" :
+                        seekMon.dispose();
+                        return reading();
+                      case "seeking text":
+                        seekMon.dispose();
+                        window.clearTimeout(idleTimeout);
+                        return reading();
                       default : return self;
                     }
                   }
@@ -147,7 +186,7 @@ module.exports =
             },
 
             reading = function(){
-                var seekMon = menuSeekingMonitor(function(){
+                var seekMon = lookingUpMonitor(function(){
                   processUIEvent("seeking menu")
                 }),
                 self = {
