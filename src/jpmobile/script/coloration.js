4a4ae2cb228a46c3c45bc70047c8461339cc27e7
Patrick Boe <patrick@hametz>
2015-11-26 16:11:40 -0500

    refined button styling

--- src/jpmobile/script/coloration.js
+++ src/jpmobile/script/coloration.js
@@ -1,13 +1,14 @@
 var dom = require('./dom');
 
 module.exports = function(form,main){
-  var colorLinks = window.sessionStorage.getItem('colorLinks') == 'true',
+  var colorLinks = window.sessionStorage.getItem('colorLinks') != 'false',
       label = form.appendChild(dom.make('<label for="coloration">Link Color Coding:</label>')),
-      button = form.appendChild(dom.make('<button id="coloration">Colorful</button>'));
+      button = form.appendChild(dom.make('<button id="coloration"></button>')),
+      colorText = '<span class="artist">C</span><span class="bodily">o</span><span class="dublin">l</span><span class="performances">o</span><span class="literature">r</span>' ;
       applyLinkColorPreference = function(){
         if(colorLinks) {
           main.classList.add('colorful');
-          button.innerHTML = 'Colorful';
+          button.innerHTML = colorText;
         } else {
           main.classList.remove('colorful');
           button.innerHTML = 'Plain';
