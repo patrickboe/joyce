eee10fb9c90304ee41b7c404d7d24d4ba9d2f9f2
Patrick Boe <patrick@hametz>
2015-12-15 03:52:57 -0500

    bump target version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jpmobile "0.1.2-SNAPSHOT"
+(defproject jpmobile "0.2.0-SNAPSHOT"
   :description "joyceproject.com responsive html redesign"
   :min-lein-version "2.0.0"
   :license {:name "GNU General Public License - Version 3"
