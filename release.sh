11e32223625d4448f835c4b8a413238d89efe7bc
Patrick Boe <patrick@hametz>
2018-10-10 16:48:29 -0400

    include script libs

--- release.sh
+++ release.sh
@@ -6,7 +6,7 @@ ZIP="target/jpmobile-${VERSION}.tar.gz"
 echo "creating release"
 lein release
 echo "zipping it up"
-tar -zc -f $ZIP *.sh *.txt *.md src target/*.jar
+tar -zc -f $ZIP *.sh *.txt *.md src target/*.jar lib/*
 echo "updating target version for next time"
 grunt version::patch
 lein do vcs commit, vcs push
